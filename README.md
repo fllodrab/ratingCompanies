40915d398d92dcbea500554ef6d6aea4c0499a87
fllodrab <llodrab@gmail.com>
2015-10-31 19:57:07 +0100

    Empezamos de nuevo el ejercicio desde 0. Ahora se pueden crear objetos en la BD de MongoDB usando Mongoose

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# ratingCompanies
-Ejercicio 2 del Tema 1 de la asignatura Cloud Computing del master en Ingeniería Informática
+express-proyecto-modular
+========================
+
+Projecto de muestra de tutorial "Node.js y Express: Estructura modular de  proyectos" en Codehero,co
--- app.js
+++ app.js
@@ -1,73 +1,39 @@
-// Dependencias
+
+/**
+ * Module dependencies.
+ */
+require('./models');
+
 var express = require('express');
 var http = require('http');
 var path = require('path');
-// hacemos referencia a la dependencia
-var mongodb = require('mongodb');
 
-// obtenemos el server MongoDB que dejamos corriendo
-// *** el puerto 27017 es el default de MongoDB
-var server = new mongodb.Server("127.0.0.1", 27017, {});
-
-// obtenemos la base de datos de prueba que creamos
-var dbTest = new mongodb.Db('unTestDB', server, {})
-
-// abrimos la base pasando el callback para cuando esté lista para usar
-dbTest.open(function (error, client) {
-  if (error) throw error;
-
-  //en el parámetro client recibimos el cliente para comenzar a hacer llamadas
-  //este parámetro sería lo mismo que hicimos por consola al llamar a mongo
-
-  //Obtenemos la coleccion personas que creamos antes
-  var collection = new mongodb.Collection(client, 'personas');
-
-  //disparamos un query buscando la persona que habiamos insertado por consola
-  collection.find({'nombre': 'pepe'}).toArray(function(err, docs) {
-
-    //imprimimos en la consola el resultado
-    console.dir(docs);
-  });
-});
-
-// Rutas
-var index = require(__dirname + '/controllers/home/index');
-
-// Crea aplicación web con Express
 var app = express();
 
-// Funcionalidades
-app.get('/', index.index);
-app.get('/crearEmpresa/:empresa', index.crearEmpresa);
-app.get('/listarCalificaciones/:empresa', index.listarCalificaciones)
-app.get('/crearCalificacion/:empresa/:alumno/:calificacion', index.crearCalificacion);
-app.get('/borrarCalificacion/:empresa/:alumno', index.borrarCalificacion);
-app.get('/actualizarCalificacion/:empresa/:alumno/:calificacion', index.actualizarCalificacion);
-app.get('/generarRanking', index.generarRanking);
+// modulos
+var home = require('./controllers/home');
+var user = require('./controllers/user');
 
-// Variables de entorno
+// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('ip', process.env.IP || '127.0.0.1');
-// Directorio con las plantillas
-app.set('views', path.join(__dirname, 'views'));
-// Motor de visualización
+app.set('views', __dirname + '/layouts');
 app.set('view engine', 'jade');
-
-//Favicon
-app.use(express.favicon(path.join(__dirname, 'public/images/favicon.ico')));
-//Logger de solicitudes HTTP
+app.use(express.favicon());
 app.use(express.logger('dev'));
-//Manejador de enrutado
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-//Página de error
-app.use(function(req, res, next) {
-  res.status(404).render('index', {
-    mensaje: 'Error 404: Página no encontrada o parámetros inválidos.'
-  });
-});
+app.use(user);
 
-// Creación del servidor
-http.createServer(app).listen(app.get('port'), app.get('ip'), function() {
-  console.log('Servidor Express escuchando en ' + app.get('ip') + ':' + app.get('port'));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// rutas
+app.use(home);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
